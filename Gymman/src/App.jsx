5617173be7d8f9cd4f69642e6155e1f86926e7a4
Rahul <rahulsaha6917@gmail.com>
2025-11-23 01:07:55 +0530

    Tasklist

--- Gymman/src/App.jsx
+++ Gymman/src/App.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react'
+import { useState,useEffect } from 'react';
 import Taskform from './Components/Taskform'
 import Tasklist from './Components/Tasklist'
 import Progresstracker from './Components/Progresstracker'
 
-export default function App() {
- const[tasks, setTasks] = useState([]);
 
-  const addTask = (task) => {
-    setTasks(task);
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  });
+
+
+  const addTask = (task)=>{
+    setTasks([...tasks, task])
+  }
+
+
+  const updateTask = (updatedTask, index) => {
+    const newtask = [...tasks];
+    newtask[index] = updatedTask;
+    setTasks(newtask);
+  }
+  const deleteTask = () => {
+
+
   }
   return (
     <div>
@@ -16,10 +34,11 @@ export default function App() {
         <p><i>Your friendly Gym Manager</i></p>
       </header>
       <Taskform addTask = {addTask}/>
-      <Tasklist />
+      <Tasklist tasks = {tasks}
+       updateTask = {updateTask} 
+       deleteTask = {deleteTask}/>
       <Progresstracker />
       <button>clear All Tasks</button>
     </div>
   )
 }
-
