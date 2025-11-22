d30c34cddcad7272ee8df0d05f185015c1d9f152
Rahul <rahulsaha6917@gmail.com>
2025-11-22 13:21:17 +0530

    Taskform Component

---
+++ Gymman/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import Taskform from './Components/Taskform'
+import Tasklist from './Components/Tasklist'
+import Progresstracker from './Components/Progresstracker'
+
+export default function App() {
+ const[tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    setTasks(task);
+  }
+  return (
+    <div>
+      <header>
+        <h1>GymMan</h1>
+        <p><i>Your friendly Gym Manager</i></p>
+      </header>
+      <Taskform addTask = {addTask}/>
+      <Tasklist />
+      <Progresstracker />
+      <button>clear All Tasks</button>
+    </div>
+  )
+}
+
---
+++ Gymman/src/Components/Tasklist.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Tasklist() {
+  return (
+    <div>
+      <h3>TaskList Component</h3>
+    </div>
+  )
+}
